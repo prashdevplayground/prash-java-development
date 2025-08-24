0a04f398a87a2a651f3c75d3d1f19b09b4a7ae7e
prashdevplayground <prash.javadev@gmail.com>
2025-08-24 14:31:14 +0530

    Delete src/main directory

--- src/main/java/com/example/inventory/dto/CategoryDtos.java
+++
@@ -1,6 +0,0 @@
-package com.example.inventory.dto;
-
-public class CategoryDtos {
-    public static class CategoryRequest { public String name; }
-    public static class CategoryResponse { public Long id; public String name; }
-}
--- src/main/java/com/example/inventory/dto/InventoryDtos.java
+++
@@ -1,8 +0,0 @@
-package com.example.inventory.dto;
-
-import java.time.Instant;
-
-public class InventoryDtos {
-    public static class TxnRequest { public Long productId; public Integer delta; }
-    public static class TxnResponse { public Long id; public Long productId; public Integer delta; public Instant at; }
-}
